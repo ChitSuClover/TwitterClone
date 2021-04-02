7b9572785957891b7ddc116ad627f3b6851544f5
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-02 21:43:10 +0630

    feat:Controller update アクションを作成,View edit 画面Pathを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -14,7 +14,16 @@ class PostsController < ApplicationController
     end
   end
   def edit
-      @post=Post.find(params[:id])
+    @post=Post.find(params[:id])
+  end
+  def update
+    @post=Post.find(params[:id])
+    if
+      @post.update(post_params)
+      redirect_to posts_path, notice: "メッセージを編集しました！"
+    else
+      render :edit
+    end
   end
   private
   def post_params
