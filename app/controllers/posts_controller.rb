f87aa04c868dd106fd555cee66ce206567c56a52
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-02 21:49:17 +0630

    feat: Controller set_post メソッドを作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+  before_action :set_post, only:[:edit, :update, :destroy]
   def index
     @posts=Post.all
   end
@@ -14,10 +15,8 @@ class PostsController < ApplicationController
     end
   end
   def edit
-    @post=Post.find(params[:id])
   end
   def update
-    @post=Post.find(params[:id])
     if
       @post.update(post_params)
       redirect_to posts_path, notice: "メッセージを編集しました！"
@@ -26,12 +25,14 @@ class PostsController < ApplicationController
     end
   end
   def destroy
-    @post=Post.find(params[:id])
     @post.destroy
     redirect_to posts_path, notice: "メッセージを削除しました！"
   end
   private
   def post_params
     params.require(:post).permit(:content)
+  end
+  def set_post
+    @post=Post.find(params[:id])
   end
 end
