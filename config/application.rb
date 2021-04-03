2f3dd221c71b18992a36c4c9f7ca7f1817bf2fcf
Chit Su Wai <chitsuwai@clovermandalay.com>
2021-04-03 21:49:25 +0630

    test:TimeZone のテスト

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module TwitterClone
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-    config.time_zone = 'Yangon'
+    config.time_zone = 'Rangoon'
     config.active_record.default_timezone = :local
 
     # Settings in config/environments/* take precedence over those specified here.
